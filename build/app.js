1ab9c5f3c160218f4dbd308ae1b32e0dcbcd181c
Luca <lucaramos99@gmail.com>
2024-07-14 03:38:11 -0300

    Add structur

---
+++ build/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _express = _interopRequireDefault(require("express"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var app = (0, _express["default"])();
+var _default = exports["default"] = app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import express from 'express'
-const app = express();
+import app from './app.js';
 
-app.listen(3000);
-console.log('Server listen on post', 3000);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listen on port ${PORT}`);
+});
