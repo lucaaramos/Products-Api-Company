b87fd4fde5eb64f82e186946eb9ebe3fa1ef0c2b
Luca <lucaramos99@gmail.com>
2024-07-14 04:09:44 -0300

    Add routes and controller for products

---
+++ build/controllers/products.controller.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.updateProduct = exports.getRelatedProducts = exports.getProductsBySearchTerm = exports.getProductsByRating = exports.getProductsByPriceRange = exports.getProductsByCategory = exports.getProductsByBrand = exports.getProductBySlug = exports.getProduct = exports.deleteProduct = exports.createProduct = void 0;
+var createProduct = exports.createProduct = function createProduct(req, res) {};
+var getProduct = exports.getProduct = function getProduct(req, res) {};
+var updateProduct = exports.updateProduct = function updateProduct(req, res) {};
+var deleteProduct = exports.deleteProduct = function deleteProduct(req, res) {};
+var getProductsByCategory = exports.getProductsByCategory = function getProductsByCategory(req, res) {};
+var getProductsByPriceRange = exports.getProductsByPriceRange = function getProductsByPriceRange(req, res) {};
+var getProductsByRating = exports.getProductsByRating = function getProductsByRating(req, res) {};
+var getProductsByBrand = exports.getProductsByBrand = function getProductsByBrand(req, res) {};
+var getProductsBySearchTerm = exports.getProductsBySearchTerm = function getProductsBySearchTerm(req, res) {};
+var getProductBySlug = exports.getProductBySlug = function getProductBySlug(req, res) {};
+var getRelatedProducts = exports.getRelatedProducts = function getRelatedProducts(req, res) {};
---
+++ src/controllers/products.controller.js
@@ -0,0 +1,44 @@
+export const  createProduct = (req,res) => {
+
+}
+
+export const  getProduct = (req,res) => {
+    res.json("--------------get products")
+}
+
+export const  updateProduct = (req,res) => {
+
+}
+
+export const  deleteProduct = (req,res) => {
+
+}
+
+export const  getProductsByCategory = (req,res) => {
+
+}
+
+export const  getProductsByPriceRange = (req,res) => {
+
+}
+
+export const  getProductsByRating = (req,res) => {
+
+}
+
+export const  getProductsByBrand = (req,res) => {
+
+}
+
+export const  getProductsBySearchTerm = (req,res) => {
+
+}
+
+export const  getProductBySlug = (req,res) => {
+
+}
+
+export const  getRelatedProducts = (req,res) => {
+
+}
+
